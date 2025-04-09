60a6e85124bb9cb89601f06eacab2fba1ab65b32
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-09 22:05:52 +0000

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
+const pronouns = ["the", "our", "my", "your"];
+const adjectives = ["great", "awesome", "crazy", "cool"];
+const nouns = ["developer", "guru", "ninja", "geek"];
+const extensions = [".com", ".net", ".io", ".dev"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function generateDomaine (){
+  for(let pronounsIndex =0; pronounsIndex<pronouns.length; pronounsIndex++){
+   console.log(pronouns[pronounsIndex]);
+   for(let adjectiveIndex =0; adjectiveIndex<adjectives.length; adjectiveIndex++){
+    console.log(adjectives[adjectiveIndex]);
+    for(let nounIndex =0; nounIndex<nouns.length; nounIndex++){
+      console.log(nouns[nounIndex]);
+    
+  }
+}
+}
+
+generateDomaine();
+}
