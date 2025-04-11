da80f10fde9a413abd530bc6b1e5b03563157503
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-11 23:49:46 +0000

    last version

--- src/app.js
+++ src/app.js
@@ -7,68 +7,62 @@ const nouns = ["developer", "guru", "ninja", "geeks"];
 const extensions = [".com", ".net", ".io", ".dev"];
 
 
-window.onload = function() {
-  let domains =[];
-  
-function generateDomaine (){
- 
-  for(let pronounsIndex =0; pronounsIndex<pronouns.length; pronounsIndex++){
-   for(let adjectiveIndex =0; adjectiveIndex<adjectives.length; adjectiveIndex++){
-    // console.log(adjectives[adjectiveIndex]);
-    for(let nounIndex =0; nounIndex<nouns.length; nounIndex++){
-      // console.log(nouns[nounIndex]);
-      for(let extensionIndex =0; extensionIndex<extensions.length; extensionIndex++){
-       domains.push(`${pronouns[pronounsIndex]} ${adjectives[adjectiveIndex]} ${nouns[nounIndex]} ${extensions[extensionIndex]}`)
-       
-       
+window.onload = function () {
+  let domains = [];
+
+  function generateDomaine() {
+
+    for (let pronounsIndex = 0; pronounsIndex < pronouns.length; pronounsIndex++) {
+      for (let adjectiveIndex = 0; adjectiveIndex < adjectives.length; adjectiveIndex++) {
+        for (let nounIndex = 0; nounIndex < nouns.length; nounIndex++) {
+          for (let extensionIndex = 0; extensionIndex < extensions.length; extensionIndex++) {
+            domains.push(`${pronouns[pronounsIndex]} ${adjectives[adjectiveIndex]} ${nouns[nounIndex]} ${extensions[extensionIndex]}`)
+
+
+          }
+        }
       }
+    }
+    return domains
   }
-}
- 
-}
-return domains
-}
+  generateDomaine()
 
-generateDomaine()
+  // for(let i=0; i<domains.length; i=i+4){
 
-const cardContainer = document.getElementById("card-container");
 
-domains.forEach(domain => {
-  // Create a card element
-  const card = document.createElement("div");
- 
+  // }
 
-  // Add card content
-  card.innerHTML = `
+
+  const cardContainer = document.getElementById("card-container");
+
+  domains.forEach(domain => {
+    // card element
+    const card = document.createElement("div");
+
+
+    //  card content
+    card.innerHTML = `
     <div class="card-body">
       <h6 class="card-title">${domain}</h6>
       
     </div>
   `;
 
-  // Append the card to the container
-  cardContainer.appendChild(card);
-  
-});
+    cardContainer.appendChild(card);
 
-console.log(domains)
+  });
+
+  // console.log(domains)
 }
 
 
 
-// function randomBgColor() {
-//         var x = Math. floor(Math. random() * 256);
-//         var y = Math. floor(Math. random() * 256);
-//         var z = Math. floor(Math. random() * 256);
-//         let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-//         console.log(bgColor);
-//         document.body.style.background = bgColor;
-//       }
 
 
 
 
-//   for (let i = 0; i < domains.length; i += 4) {}
+
+
 
 
 
